0718a1124ba040d40acb2cb68d239f79d76018d5
Samuel <sramirezn1@eafit.edu.co>
2026-08-03 22:35:26 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Tu Nombre Aqui'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
